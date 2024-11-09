09827f757dd30ce31811309ec614d6f67e59ec2d
Jack Ojango <129906717+JacksCodeVault@users.noreply.github.com>
2024-11-09 23:50:08 +0300

    Update .gitignore to include firebase-config.js and add Firebase configuration details

--- web/src/config/firebase-config.js
+++ web/src/config/firebase-config.js
@@ -2,7 +2,13 @@ import { initializeApp } from 'firebase/app';
 import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 
 const firebaseConfig = {
-  
+  apiKey: "AIzaSyDJbgmMRBU8ifozFHo4jaYHeH6cObAglAo",
+  authDomain: "sms-sync-bridge.firebaseapp.com",
+  databaseURL: "https://sms-sync-bridge-default-rtdb.firebaseio.com",
+  projectId: "sms-sync-bridge",
+  storageBucket: "sms-sync-bridge.firebasestorage.app",
+  messagingSenderId: "470504583266",
+  appId: "1:470504583266:web:450fc945efbe9fe566613b"
 };
 
 const app = initializeApp(firebaseConfig);
